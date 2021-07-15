70b6ad053fd14630385c9e469aaa1f8b596e2685
Justin Guese <guese.justin@gmail.com>
2021-07-15 15:23:07 +0200

    fix for kubernetes env paste

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,7 @@
 set -x
 
 /app/scripts/wait-for-it.sh $POSTGRES_HOST:5432 -- echo "db is up"
-npx prisma $POSTGRES_HOST push
+export DATABASE_URL="postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:5432/$POSTGRES_DB"
+echo $DATABASE_URL
+npx prisma db push
 yarn start
